eba1e8826e3fabaf32abf686865f16d9a4c0910a
Saurabh Patel <142709343+Itssaurabhpatel@users.noreply.github.com>
2024-09-26 12:51:12 +0530

    Add files via upload

---
+++ 15_Docker/PyProject/myapp.py
@@ -0,0 +1,7 @@
+print("Program to sum two numbers")
+num1 = int (input('Enter the First no:'))
+num2 = int (input('Enter the Second no:'))
+
+result = num1 + num2
+
+print("Sum of two numbers is :",result)
